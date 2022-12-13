6692424c2d6683004468ad5fe03f5e92a28c88c2
Tamonash <tamonashbhattacharyya2@gmail.com>
2022-12-14 02:54:17 +0530

    Distance Comparator Sample

--- server/pyscripts/Retrieve.py
+++ server/pyscripts/Retrieve.py
@@ -30,19 +30,38 @@ def getDataFromDonors():
     client = MongoClient("mongodb+srv://admin:12345@almquest.toauhu5.mongodb.net/?retryWrites=true&w=majority",
                          tls=True,
                          tlsAllowInvalidCertificates=True)
-    database = client["almquest"]
-    collection = database["donors"]
+    database1 = client["almquest"]
+    collection1 = database1["donors"]
+    collection2 = database1["distributors"]
 
-    result = collection.find()
+    donors = collection1.find()
+    distributors = collection2.find()
 
-    for res in result:
-        print('name: ', res['name'], 'lat: ', res['location']['coordinates'][0], "\nlon: ", res['location']
+    for res in donors:
+        print("----------------------------------")
+        print('name: ', res['name'], '\nlat: ', res['location']['coordinates'][0], "\nlon: ", res['location']
         ['coordinates'][1])
+        lat1 = res['location']['coordinates'][0]
+        lon1 = res['location']['coordinates'][1]
+        print("Distance from each Distributor: \n")
+        distributors = collection2.find()
+        for res2 in distributors:
+            lat2 = res2['location']['coordinates'][0]
+            lon2 = res2['location']['coordinates'][1]
+            distance = haversine((lat1, lon1), (lat2, lon2))
+            print("     ", "Name: ", res2['name'])
+            print("     ", "Distance: ", distance, " kms")
+            print("---------")
+
+
+
 
 
 def main():
+    print("\nDistributors: \n")
     getDataFromDistributors()
-    # getDataFromDonors()
+    print("\n\nDonors: \n")
+    getDataFromDonors()
 
 
 if __name__ == "__main__":
