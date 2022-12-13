b8a84c74aacb2735bb10232ad19fa06d377f82ac
intelligent-psychopath <79144740+intelligent-psychopath@users.noreply.github.com>
2022-12-13 18:47:49 +0530

    Added Basic Navbar and Footer Components

--- almquest-client/src/App.jsx
+++ almquest-client/src/App.jsx
@@ -1,65 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { useGoogleLogin } from '@react-oauth/google';
-import axios from 'axios';
-import "./App.css";
+import React from "react";
+import Navbar from "./components/Common/Navbar";
+import Footer from "./components/Common/Footer";
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  const login = useGoogleLogin({
-    onSuccess: async (response) => {
-      try {
-        const res = await axios.get("https://www.googleapis.com/oauth2/v3/userinfo", {
-          headers: {
-            "Authorization": `Bearer ${response.access_token}`
-          }
-        });
-
-        console.log(res.data);
-        const { name, email } = res.data;
-      } catch (err) {
-        console.log(err);
-      }
-    }
-  });
-
+const App = () => {
   return (
-    <div className="bg-white h-[100vh] dark:bg-gray-700 overflow-auto mx-auto">
-      <div className="my-[2.5vh] w-full md:w-2/3 mx-auto bg-white dark:bg-gray-900 rounded-lg">
-        <section className="flex flex-col items-center justify-center px-6 mx-auto">
-          <div className="flex items-center h-[80vh]">
-            <button
-              onClick = { login }
-              type="button"
-              className="flex items-center justify-center px-6 py-3 text-gray-600 transition-colors duration-300 transform border rounded-lg dark:border-gray-700 dark:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600"
-            >
-              <svg className="w-6 h-6 mx-2" viewBox="0 0 40 40">
-                <path
-                  d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
-                  fill="#FFC107"
-                />
-                <path
-                  d="M5.25497 12.2425L10.7308 16.2583C12.2125 12.59 15.8008 9.99999 20 9.99999C22.5491 9.99999 24.8683 10.9617 26.6341 12.5325L31.3483 7.81833C28.3716 5.04416 24.39 3.33333 20 3.33333C13.5983 3.33333 8.04663 6.94749 5.25497 12.2425Z"
-                  fill="#FF3D00"
-                />
-                <path
-                  d="M20 36.6667C24.305 36.6667 28.2167 35.0192 31.1742 32.34L26.0159 27.975C24.3425 29.2425 22.2625 30 20 30C15.665 30 11.9842 27.2359 10.5975 23.3784L5.16254 27.5659C7.92087 32.9634 13.5225 36.6667 20 36.6667Z"
-                  fill="#4CAF50"
-                />
-                <path
-                  d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.7592 25.1975 27.56 26.805 26.0133 27.9758C26.0142 27.975 26.015 27.975 26.0158 27.9742L31.1742 32.3392C30.8092 32.6708 36.6667 28.3333 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
-                  fill="#1976D2"
-                />
-              </svg>
-
-              <span className="mx-2">Sign in with Google</span>
-            </button>
-          </div>
-        </section>
-      </div>
-    </div>
+    <>
+      <Navbar />
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
