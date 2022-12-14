d1b283695741924b412894d8a594f601382d7802
Aditya Das <adityabkp1234@gmail.com>
2022-12-14 12:56:12 +0530

    PythonShell

---
+++ server/pythonScript/check.py
@@ -0,0 +1,26 @@
+import sys
+import time
+
+name = sys.argv[1]
+name = name + "Squad"
+
+time.sleep(1)
+print(name)
+
+time.sleep(2)
+name = name + " AD"
+
+print(name)
+
+# Let input from server
+# name = Quad 
+
+# perform all operations
+
+# output array = []
+# every time you "print", data is pushed into output array
+
+# after program terminates, entire output array is returned 
+# to server as result  
+
+# so here, result => [ "QuadSquad", "QuadSquad AD" ]
