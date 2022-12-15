1e13934d45dcdfbd4fb4d6ea533ab6b18710f29b
Tamonash <tamonashbhattacharyya2@gmail.com>
2022-12-16 00:03:54 +0530

    Coordinate values

---
+++ server/pyscripts/CoordinateFromAddress.py
@@ -0,0 +1,18 @@
+import geopandas
+
+
+def pointsFromAddress(add1):
+    df = geopandas.tools.geocode([add1])
+    return df
+
+
+def point():
+    add = "27A,College Rd, Shalimar, Howrah, West Bengal 711103"
+    add2 = "Botanical Garden Area, Kolkata, West Bengal 711103"
+    x = pointsFromAddress(add2)
+    print(x['geometry'])
+
+
+point()
+
+
