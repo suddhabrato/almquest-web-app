afc7efe4105f9ef1f32e1e8bb7608da6eb064aa4
Tamonash <tamonashbhattacharyya2@gmail.com>
2022-12-23 23:20:38 +0530

    Pairing.py fixed 1

--- almquest-client/src/components/Common/Navbar/index.jsx
+++ almquest-client/src/components/Common/Navbar/index.jsx
@@ -141,19 +141,19 @@ const Navbar = ({ darkMode, toggleDarkMode }) => {
                 href="#"
                 className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
               >
-                Join Slack
+                Features
               </a>
               <a
                 href="#"
                 className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
               >
-                Browse Topics
+                About Us
               </a>
               <a
                 href="#"
                 className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
               >
-                Random Item
+                Team
               </a>
               <Link
                 to="/contact"
