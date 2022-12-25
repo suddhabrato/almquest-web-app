c1cc9034320ba6ffdfd8d384d89ed2bd2252df2b
Aditya Das <adityabkp1234@gmail.com>
2022-12-25 22:39:45 +0530

    schema change

--- server/models/donatedPackages.js
+++ server/models/donatedPackages.js
@@ -6,6 +6,8 @@ const donatedSchema = new mongoose.Schema({
     ref: "Donor",
     required: true,
   },
+  donor_name: String,
+  donor_phone: String,
   quantity: {
     type: Number,
     required: true,
@@ -32,10 +34,22 @@ const donatedSchema = new mongoose.Schema({
     default: "Not Paired",
     required: true,
   },
+  timestamp: Date,
   pair: {
     distributor_id: {
       type: mongoose.Schema.ObjectId,
       ref: "Distributor",
+    },
+    distributor_name: String,
+    distributor_phone: String,
+    distributor_location: {
+      type: {
+        type: String,
+        default: "Point",
+        enum: ["Point"],
+      },
+      coordinates: [Number],
+      address: String,
     },
     meet_location: {
       type: {
