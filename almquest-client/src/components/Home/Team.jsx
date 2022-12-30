9a15b39a5b8bdfb43146210e9bbb65599ef2ee8a
Tamonash <tamonashbhattacharyya2@gmail.com>
2022-12-30 12:23:58 +0530

    team content 1

--- almquest-client/src/components/Home/Team.jsx
+++ almquest-client/src/components/Home/Team.jsx
@@ -16,9 +16,8 @@ const Team = ({ pageRefs }) => {
           <span className="inline-block w-1 h-1 bg-blue-500 rounded-full"></span>
         </div>
         <p className="max-w-2xl mx-auto my-6 text-center text-gray-500 dark:text-gray-300">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo incidunt
-          ex placeat modi magni quia error alias, adipisci rem similique, at
-          omnis eligendi optio eos harum.
+          We are the team that has ideated and developed AlmQuest with the sole intent of adding some 
+          value to the society and hopefully making some lives better. 
         </p>
 
         <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 xl:grid-cols-4">
