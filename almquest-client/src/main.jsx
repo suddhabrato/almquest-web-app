d6d6cbbf4d5903bc9c5812a2e93761787c972ad0
Tamonash <tamonashbhattacharyya2@gmail.com>
2023-01-16 20:18:19 +0530

    server changed

--- almquest-client/src/main.jsx
+++ almquest-client/src/main.jsx
@@ -10,7 +10,7 @@ import { UserContextProvider } from "./contexts/UserContext";
 import { BrowserRouter } from "react-router-dom";
 
 // axios.defaults.baseURL = "http://localhost:3000";
-axios.defaults.baseURL = "https://almquest-server.onrender.com";
+axios.defaults.baseURL = "https://almquest-server-e0jb.onrender.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <GoogleOAuthProvider clientId="31211872217-l7030vqhkrmsahh0p8q7lrsc7h4u7vj7.apps.googleusercontent.com">
